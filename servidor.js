e1fb4177ac38cbaa93ea55fafd3abb4c0e64b49f
Thiago <cardozothiago07@gmail.com>
2021-11-27 15:22:09 -0300

    criando inicio dos servidores

--- servidor.js
+++ servidor.js
@@ -2,27 +2,72 @@ const Graph = require("./Structure/graph");
 const Node = require("./Structure/node");
 const Link = require("./Structure/link");
 const fs = require("fs");
-const path = require("path/posix");
+const express = require("express");
+const net = require("net");
 
-//var coordinator = false;
+const IP_HTTP = "127.0.0.1";
+let PORT_HTTP = 8000;
+
+const IP_TCP = "127.0.0.1";
+let PORT_TCP = 8080;
+
+switch (process.argv[2]) {
+  case "A":
+    PORT_HTTP = 7000;
+    PORT_TCP = 7080;
+    break;
+  case "B":
+    PORT_HTTP = 8000;
+    PORT_TCP = 8080;
+    break;
+  case "C":
+    PORT_HTTP = 9000;
+    PORT_TCP = 9080;
+    break;
+}
+
+const serversAdress = {
+  A: { ip: "127.0.0.1", port: 7080 },
+  B: { ip: "127.0.0.1", port: 8080 },
+  C: { ip: "127.0.0.1", port: 9080 },
+};
+let coordinator = false;
+let graphForSend;
 
 graph = new Graph();
 readGraph(process.argv[2], graph);
-console.log("Grafo :" + process.argv[2] + ":", graph);
-findPath(graph); //retorna um array de strings com todos os caminhos, para passar para a interface.
+
+//Rotas da interface
+const app = express();
+app.post("/searchRoutes", function (req, res) {
+  origin = req.origin;
+  destination = req.destination;
+});
+
+app.post("/purchaseRoute", function (req, res) {});
+
+app.listen(PORT_HTTP, IP_HTTP, () => {
+  console.log("Servidor HTTP =", IP_HTTP + ":" + PORT_HTTP);
+});
+
+//Servidor TCP
+const server = net.createServer((socket) => {});
+
+server.listen(PORT_TCP, IP_TCP, () => {
+  console.log("Servidor TCP =", IP_TCP + ":" + PORT_TCP);
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                         FUNÇÕES
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//findPath(graph); //retorna um array de strings com todos os caminhos, para passar para a interface.
 
 //ler o grafo do arquivo, agrupar grafo, buscar caminho, reservar vagas, função de eleição.
-function randInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 function readGraph(text, graph) {
   var graphInfo;
   let links = [];
-  data = fs.readFileSync("./files/grafo" + text + ".txt", "utf8");
-  graphInfo = data.replace(/(\r\n|\n|\r)/gm, ",").split(",");
+  graphForSend = fs.readFileSync("./files/grafo" + text + ".txt", "utf8");
+  graphInfo = graphForSend.replace(/(\r\n|\n|\r)/gm, ",").split(",");
 
   for (let index = 0; index < graphInfo.length; index++) {
     switch (index % 4) {
@@ -54,9 +99,9 @@ function groupGraph() {
   //conexão entre os sockets, compartilhamento e junção dos grafos.
 }
 
-function findPath(graph) {
-  let prev = graph.findRoute(5, 7, 10);
-  path = graph.reconstruct(5, 7, prev);
+function findPath(graph, originId, destinationId) {
+  let prev = graph.findRoute(originId, destinationId, 10);
+  path = graph.reconstruct(originId, destinationId, prev);
   return path;
 }
 
@@ -71,5 +116,3 @@ function elect() {
   //Perguntar se existe algum coordenador, caso não iniciar uma eleição.
   //criar função de timeout por coordenador(trocar após N requisições respondidas)
 }
-
-//Rotas da interface
